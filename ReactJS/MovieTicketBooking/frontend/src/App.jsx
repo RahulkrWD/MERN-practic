31ef3326070a9622737785328e80ce3bcfaa3bec
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-11-13 00:35:01 +0530

    add react

---
+++ ReactJS/MovieTicketBooking/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Components/static/Navbar";
+function App(){
+return (
+
+  <div>
+  <Navbar/>
+    <h3>App.jsx</h3>
+
+  </div>
+)
+
+}
+export default App;
---
+++ ReactJS/MovieTicketBooking/frontend/src/Components/static/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+function Navbar(){
+    return (
+
+        <div className="navbar">
+        <div className="">
+        <h3>BookTKT</h3>
+        <input placeholder="Movie, series, standup show." />
+        </div>
+        <div>
+        <h5>Location</h5>
+       <button  className="btn text-bg-danger">Signup</button>
+       <h5>lcons</h5>
+
+        </div>
+        </div>
+    )
+}
+
+export default Navbar;
